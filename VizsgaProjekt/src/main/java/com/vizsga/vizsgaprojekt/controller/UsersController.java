f678d2c5f42646a7ae5a6a0f4cc37806284130a4
Simkincs <simonkincso2002@gmail.com>
2024-12-12 13:04:08 +0100

    JWT token

---
+++ VizsgaProjekt/src/main/java/com/vizsga/vizsgaprojekt/controller/UsersController.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/GenericResource.java to edit this template
+ */
+package com.vizsga.vizsgaprojekt.controller;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author asd
+ */
+@Path("users")
+public class UsersController {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of UsersController
+     */
+    public UsersController() {
+    }
+
+    /**
+     * Retrieves representation of an instance of com.vizsga.vizsgaprojekt.controller.UsersController
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public String getXml() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of UsersController
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    public void putXml(String content) {
+    }
+}
