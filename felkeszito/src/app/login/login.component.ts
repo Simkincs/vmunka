09cb4e06c4512d63154dfa2ae8bde63837ff1332
ILoveMeggy <90890724+ILoveMeggy@users.noreply.github.com>
2025-02-23 15:46:29 +0100

    test

---
+++ felkeszito/src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule], // FormsModule IMPORTÁLÁSA!
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor(private router: Router) {}
+
+  login() {
+    if (this.email.trim() === '' || this.password.trim() === '') {
+      this.errorMessage = 'Töltse ki az összes mezőt!';
+    } else {
+      this.errorMessage = '';
+      console.log("Sikeres bejelentkezés, átirányítás a /home oldalra");
+      this.router.navigate(['/home']);
+    }
+  }
+
+  navigateToRegister() {
+    this.router.navigate(['/register']);
+  }
+}
